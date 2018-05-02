a0e5bc895d3d9895a04948caefdb4603764ab0ab
Samuel <samu.rcosta@gmail.com>
2018-05-02 18:45:38 -0300

    Sending the app

---
+++ app/src/main/java/com/example/samue/cartaovisita/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.samue.cartaovisita;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sair(View v) {
+        finish();
+        System.exit(0);
+    }
+}
